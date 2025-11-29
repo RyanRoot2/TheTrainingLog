28dc335c8d07a4bc13397b20f44ed7d793ea58f6
RyanRoot2 <127535709+RyanRoot2@users.noreply.github.com>
2025-11-29 23:05:43 +0000

    commit

--- app.py
+++ app.py
@@ -18,6 +18,21 @@ if not st.session_state.authenticated:
 
 st.set_page_config(layout="wide")
 
+st.markdown("""
+<style>
+.stMainBlockContainer {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+}
+.stAppHeader {
+    background-color: rgba(255, 255, 255, 0.0);
+    visibility: visible;
+}
+</style>
+""", unsafe_allow_html=True)
+
 with open('programs/mass_impact.json') as f:
     program = json.load(f)
 
