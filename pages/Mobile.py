1f4c498a8b720b38703120d76240adcd70dd45c7
RyanRoot2 <127535709+RyanRoot2@users.noreply.github.com>
2025-12-04 22:20:26 +0000

    mobile view

--- pages/Mobile.py
+++ pages/Mobile.py
@@ -6,25 +6,32 @@ from src.ui.navigation import render_grid, render_movements_and_sets
 
 check_st_authentication()
 
-if 'current_screen' not in st.session_state:
-    st.session_state.current_screen = 'week_select'
+week_selected = st.session_state.get('selected_week')
+day_selected = st.session_state.get('selected_day')
 
-if 'selected_week' in st.session_state and 'selected_day' not in st.session_state:
+if day_selected is not None:
+    st.session_state.current_screen = 'day_view'
+
+elif week_selected is not None:
     st.session_state.current_screen = 'day_select'
 
-if 'selected_week' and 'selected_day' in st.session_state:
-    st.session_state.current_screen = 'day_view'
+else:
+    st.session_state.current_screen = 'week_select'
+
 
 
 st.write(st.session_state.current_screen)
 
 
+
 if st.session_state.current_screen == 'week_select':
-    render_grid("Week", program["weeks"], 3, 'day_select') 
+    render_grid("Week", program["weeks"], 3, 'day_select')
+
 elif st.session_state.current_screen == 'day_select':
     selected_week_num = st.session_state.selected_week
     program_week = program["weeks"][f"week_{selected_week_num}"]
     render_grid("Day", program_week, 1, 'day_view')
+    
 elif st.session_state.current_screen == 'day_view':
     selected_week_num = st.session_state.selected_week
     selected_day_num = st.session_state.selected_day
