4613bdaee39574e0159a0f79545317847f2e6d88
RyanRoot2 <127535709+RyanRoot2@users.noreply.github.com>
2025-12-07 17:34:07 +0000

    Added google oauth

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ from src.utils.streamlit_temp_auth import check_st_authentication
 from src.core.state import initialise_state
 
 
-check_st_authentication()
-initialise_state()
-st.write(st.session_state)
+def show_login_button():
+    # If the user is NOT logged in (st.user is not authenticated)
+    if not st.user.is_logged_in:
+        st.title("Secure App Demo")
+        st.info("👋 Please log in to continue.")
+        
+        # Display the login button, which triggers the OIDC flow
+        if st.button("Log in with Google"):
+            st.login() 
+        
+        # Prevent the rest of the application from running
+        st.stop()
+show_login_button()
 
-st.button("Go to next workout... TBC.")
+if st.button("Log out"):
+    st.logout()
+st.write(f"Hello, {st.user.name}!")
+
+initialise_state()
+st.info("👋 Hello!")
+st.button("Go to next workout... TBC")
 
 
 
